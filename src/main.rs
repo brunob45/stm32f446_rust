59d1965e00441e2a5044fd81e2f24f8d8f5ab625
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-04-07 23:14:39 -0400

    use signal for blinking in another task

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,42 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+enum SomeCommand {
+    On,
+    Off,
+}
+
+static SOME_SIGNAL: Signal<ThreadModeRawMutex, SomeCommand> = Signal::new();
+
+#[embassy_executor::task]
+async fn blink(pin: AnyPin) {
+    let mut led = Output::new(pin, Level::High, Speed::Low);
+
+    loop {
+        match SOME_SIGNAL.wait().await {
+            SomeCommand::Off => led.set_low(),
+            SomeCommand::On => led.set_high(),
+        };
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let mut led = Output::new(p.PB2, Level::High, Speed::Low);
+    // Spawned tasks run in the background, concurrently.
+    spawner.spawn(blink(p.PB2.degrade())).unwrap();
 
     loop {
-        info!("high");
-        led.set_high();
         Timer::after_millis(300).await;
-
-        info!("low");
-        led.set_low();
+        SOME_SIGNAL.signal(SomeCommand::On);
         Timer::after_millis(300).await;
+        SOME_SIGNAL.signal(SomeCommand::Off);
     }
 }
