bdf7dcb9f6270cb8603d59ecb49c5f4e2fded27c
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-04-07 23:29:00 -0400

    use 168MHz PLL, begin semihosting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+// use cortex_m_semihosting::hprint;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
+use embassy_stm32::{time::Hertz, Config};
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
@@ -29,8 +30,28 @@ async fn blink(pin: AnyPin) {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL168,
+            divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
+            divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV4;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+        config.rcc.sys = Sysclk::PLL1_P;
+    }
+    let p = embassy_stm32::init(config);
+    // hprint!("Hello World!").unwrap();
 
     // Spawned tasks run in the background, concurrently.
     spawner.spawn(blink(p.PB2.degrade())).unwrap();
