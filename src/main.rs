e965a534762614d537b1713a61f9c6c6a5b4acdb
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-04-09 23:51:26 -0400

    play with channels and tasks

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,47 @@ use cortex_m::asm;
 use cortex_m_semihosting::hprintln;
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
+use embassy_stm32::gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed};
 // use embassy_stm32::time::Hertz;
-use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
-use embassy_time::{Duration, Ticker};
+use embassy_stm32::exti::{AnyChannel, Channel, ExtiInput};
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, channel, signal};
+use embassy_time::{Duration, Instant, Ticker};
 
 use {defmt_rtt as _, panic_probe as _};
 
-enum SomeCommand {
-    On,
-    Off,
-}
+// enum SomeCommand {
+//     On,
+//     Off,
+// }
 
-type LedSignal = Signal<ThreadModeRawMutex, SomeCommand>;
+// type LedSignal = signal::Signal<ThreadModeRawMutex, SomeCommand>;
+type TriggerChannel = channel::Channel<ThreadModeRawMutex, Instant, 8>;
+type SpeedChannel = channel::Channel<ThreadModeRawMutex, Duration, 8>;
 
 #[embassy_executor::task]
-async fn blink(pin: AnyPin, signal: &'static LedSignal) {
-    let mut led = Output::new(pin, Level::High, Speed::Low);
+async fn blink(sync_ch: &'static TriggerChannel, speed_ch: &'static SpeedChannel) {
+    let mut last = Instant::from_ticks(0);
 
     loop {
-        match signal.wait().await {
-            SomeCommand::Off => led.set_low(),
-            SomeCommand::On => led.set_high(),
-        };
+        let new = sync_ch.receive().await;
+        speed_ch.send(new - last).await;
+        last = new;
+    }
+}
+
+#[embassy_executor::task]
+async fn trigger(pin: AnyPin, exti_ch: AnyChannel, speed_ch: &'static SpeedChannel) {
+    let mut _button = ExtiInput::new(Input::new(pin, Pull::Down), exti_ch);
+
+    let n_teeth = 24;
+    let n_degrees = 720;
+    let deg_per_teeth = n_degrees / n_teeth;
+
+    loop {
+        let res = speed_ch.receive().await;
+        let period_us = res.as_micros() as u32;
+        let _speed = 500_000 * deg_per_teeth / (3 * period_us);
+        asm::nop();
     }
 }
 
@@ -39,19 +57,44 @@ async fn main(spawner: Spawner) {
 
     hprintln!("Hello World!");
 
-    static LED_SIGNAL: LedSignal = LedSignal::new();
+    // static LED_SIGNAL: LedSignal = LedSignal::new();
+    static TRIGGER_CHANNEL: TriggerChannel = channel::Channel::new();
+    static SPEED_CHANNEL: SpeedChannel = channel::Channel::new();
 
     // Spawned tasks run in the background, concurrently.
-    spawner.spawn(blink(p.PB2.degrade(), &LED_SIGNAL)).unwrap();
+    spawner
+        .spawn(blink(&TRIGGER_CHANNEL, &SPEED_CHANNEL))
+        .unwrap();
+    spawner
+        .spawn(trigger(
+            p.PC13.degrade(),
+            p.EXTI13.degrade(),
+            &SPEED_CHANNEL,
+        ))
+        .unwrap();
 
-    let mut ticker = Ticker::every(Duration::from_millis(500));
+    let mut ticker = Ticker::every(Duration::from_millis(42));
+
+    let mut cpt = 0u8;
+    let mut led = Output::new(p.PB2, Level::High, Speed::Low);
 
     loop {
         ticker.next().await;
-        LED_SIGNAL.signal(SomeCommand::On);
+        led.set_low();
 
         ticker.next().await;
-        LED_SIGNAL.signal(SomeCommand::Off);
+
+        if cpt < 23 {
+            led.set_high();
+            TRIGGER_CHANNEL.send(Instant::now()).await;
+            cpt = cpt + 1;
+        } else {
+            cpt = 0;
+        }
+        // LED_SIGNAL.signal(SomeCommand::On);
+
+        // ticker.next().await;
+        // LED_SIGNAL.signal(SomeCommand::Off);
     }
 }
 
