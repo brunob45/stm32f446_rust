73c01f66fdb19ff20e428108d7b6fcd8c10fc05e
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-04-20 10:40:51 -0400

    improve fclk test

--- src/main.rs
+++ src/main.rs
@@ -5,76 +5,84 @@ use cortex_m::asm;
 use cortex_m_semihosting::hprintln;
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_stm32::time::Hertz;
-use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
+// use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 use embassy_time::{Duration, Instant, Ticker};
 
 use {defmt_rtt as _, panic_probe as _};
 
-enum SomeCommand {
-    On,
-    Off,
-}
-
-type LedSignal = Signal<ThreadModeRawMutex, SomeCommand>;
-
-#[embassy_executor::task]
-async fn blink(pin: AnyPin, signal: &'static LedSignal) {
-    let mut led = Output::new(pin, Level::High, Speed::Low);
-
-    loop {
-        match signal.wait().await {
-            SomeCommand::Off => led.set_low(),
-            SomeCommand::On => led.set_high(),
-        };
-    }
-}
-
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = peripheral_config();
 
     hprintln!("Hello World!");
 
-    static LED_SIGNAL: LedSignal = LedSignal::new();
+    let mut led = Output::new(p.PB2, Level::High, Speed::Low);
 
-    // Spawned tasks run in the background, concurrently.
-    spawner.spawn(blink(p.PB2.degrade(), &LED_SIGNAL)).unwrap();
+    let begin = Instant::now().as_micros();
+    for _ in 0..1000000 {
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+    }
+    let time1 = Instant::now().as_micros() - begin;
+
+    let begin = Instant::now().as_micros();
+    for _ in 0..1000000 {
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+    }
+    let time2 = Instant::now().as_micros() - begin;
+
+    let begin = Instant::now().as_micros();
+    for _ in 0..1000000 {
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+    }
+    let time4 = Instant::now().as_micros() - begin;
+
+    let begin = Instant::now().as_micros();
+    for _ in 0..1000000 {
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+        asm::nop();
+    }
+    let time8 = Instant::now().as_micros() - begin;
+    hprintln!("1 {}", time1);
+    hprintln!("2 {}", time2);
+    hprintln!("4 {}", time4);
+    hprintln!("8 {}", time8);
 
     let mut ticker = Ticker::every(Duration::from_millis(500));
-
-    let begin = Instant::now().as_micros();
-    for _ in 0..1000000 {
-        asm::nop();
-    }
-    let end = Instant::now().as_micros();
-    hprintln!("{} - {} = {}", end, begin, end - begin);
-
-    let begin = Instant::now().as_micros();
-    for _ in 0..1000000 {
-        asm::nop();
-        asm::nop();
-    }
-    let end = Instant::now().as_micros();
-    hprintln!("{} - {} = {}", end, begin, end - begin);
-
-    let begin = Instant::now().as_micros();
-    for _ in 0..1000000 {
-        asm::nop();
-        asm::nop();
-        asm::nop();
-        asm::nop();
-    }
-    let end = Instant::now().as_micros();
-    hprintln!("{} - {} = {}", end, begin, end - begin);
-
     loop {
         ticker.next().await;
-        LED_SIGNAL.signal(SomeCommand::On);
+        led.set_high();
 
         ticker.next().await;
-        LED_SIGNAL.signal(SomeCommand::Off);
+        led.set_low();
     }
 }
 
@@ -83,20 +91,32 @@ fn peripheral_config() -> embassy_stm32::Peripherals {
 
     let mut config = embassy_stm32::Config::default();
 
-    config.rcc.hsi = true;
     config.rcc.hse = Some(Hse {
         freq: Hertz(8_000_000),
         mode: HseMode::Oscillator,
     });
-    config.rcc.sys = Sysclk::PLL1_P;
-    config.rcc.pll_src = PllSource::HSE;
+
     config.rcc.pll = Some(Pll {
         prediv: PllPreDiv::DIV4,
         mul: PllMul::MUL168,
-        divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
-        divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
-        divr: None,
+        divp: Some(PllPDiv::DIV2),
+        divq: Some(PllQDiv::DIV7),
+        divr: Some(PllRDiv::DIV2),
     });
+
+    // automatic configuration of PLL Source Mux
+    config.rcc.pll_src = match config.rcc.hse {
+        Some(_) => PllSource::HSE,
+        None => PllSource::HSI,
+    };
+
+    // automatic configuration of System Clock Mux
+    config.rcc.sys = match (config.rcc.pll, config.rcc.hse) {
+        (None, None) => Sysclk::HSI,
+        (None, _) => Sysclk::HSE,
+        (_, _) => Sysclk::PLL1_P,
+    };
+
     config.rcc.ahb_pre = AHBPrescaler::DIV1;
     config.rcc.apb1_pre = APBPrescaler::DIV4;
     config.rcc.apb2_pre = APBPrescaler::DIV2;
