8309196c580d2ae653c85f5970074e0363ada291
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-10-14 15:34:27 -0400

    make usb work

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,44 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    time::Hertz,
+    Config,
+};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+mod usb;
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
+
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL168,
+            divp: Some(PllPDiv::DIV2), // 8mhz / 4 * 168 / 2 = 168Mhz.
+            divq: Some(PllQDiv::DIV7), // 8mhz / 4 * 168 / 7 = 48Mhz.
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV4;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
+    }
+    let p = embassy_stm32::init(config);
+
+    usb::usb_init(p.USB_OTG_FS, p.PA12, p.PA11).await;
 
     let mut led = Output::new(p.PB2, Level::High, Speed::Low);
 
