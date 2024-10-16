9f26df36d134d9935be6fd94f1812feae5b0726e
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-10-16 01:00:02 -0400

    begin sd fat fs

--- src/sdmmc.rs
+++ src/sdmmc.rs
@@ -1,16 +1,90 @@
 use defmt::*;
-use embassy_stm32::sdmmc::{DataBlock, Sdmmc};
+use embassy_stm32::sdmmc::Sdmmc;
 use embassy_stm32::time::mhz;
 use embassy_stm32::{bind_interrupts, peripherals, sdmmc};
 use {defmt_rtt as _, panic_probe as _};
 
-/// This is a safeguard to not overwrite any data on the SD card.
-/// If you don't care about SD card contents, set this to `true` to test writes.
-const ALLOW_WRITES: bool = true;
-
 bind_interrupts!(struct Irqs {
     SDIO => sdmmc::InterruptHandler<peripherals::SDIO>;
 });
+
+struct MySdmmc<'d> {
+    inner: Sdmmc<'d, peripherals::SDIO, peripherals::DMA2_CH3>,
+}
+
+impl<'d> MySdmmc<'d> {
+    pub fn new(sdmmc: Sdmmc<'d, peripherals::SDIO, peripherals::DMA2_CH3>) -> Self {
+        Self { inner: sdmmc }
+    }
+}
+
+impl<'d> embedded_sdmmc::BlockDevice for MySdmmc<'d> {
+    type Error = ();
+
+    fn num_blocks(&self) -> Result<embedded_sdmmc::BlockCount, Self::Error> {
+        Ok(embedded_sdmmc::BlockCount(0))
+    }
+
+    fn read(
+        &mut self,
+        blocks: &mut [embedded_sdmmc::Block],
+        start_block_idx: embedded_sdmmc::BlockIdx,
+    ) -> Result<(), Self::Error> {
+        let mut buffer = embassy_stm32::sdmmc::DataBlock { 0: [0; 512] };
+        for i in 0..blocks.len() {
+            embassy_futures::block_on(self.inner.read_block(start_block_idx.0 + (i as u32), &mut buffer)).map_err(|_| ())?;
+            blocks[i].contents.clone_from_slice(&buffer.0);
+        }
+        Ok(())
+    }
+
+    fn write(
+        &mut self,
+        blocks: &[embedded_sdmmc::Block],
+        start_block_idx: embedded_sdmmc::BlockIdx,
+    ) -> Result<(), Self::Error> {
+        let mut buffer = embassy_stm32::sdmmc::DataBlock { 0: [0; 512] };
+        for i in 0..blocks.len() {
+            buffer.0.clone_from_slice(&blocks[i].contents);
+            embassy_futures::block_on(self.inner.write_block(start_block_idx.0 + (i as u32), &buffer)).map_err(|_| ())?
+        }
+        Ok(())
+    }
+}
+
+struct MyTs {}
+
+impl embedded_sdmmc::TimeSource for MyTs {
+    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
+        embedded_sdmmc::Timestamp {
+            year_since_1970: 0,
+            zero_indexed_month: 0,
+            zero_indexed_day: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }
+    }
+}
+
+fn create_file(vm: &mut embedded_sdmmc::VolumeManager<MySdmmc, MyTs>) -> Result<(), ()> {
+    let mut v0 = match vm.open_volume(embedded_sdmmc::VolumeIdx(0)) {
+        Ok(x) => x,
+        Err(_) => return Err(()),
+    };
+    info!("Volume created");
+    let mut root = match v0.open_root_dir() {
+        Ok(x) => x,
+        Err(_) => return Err(()),
+    };
+    info!("Root dir created");
+    let _my_file = match root.open_file_in_dir("TEST.TXT", embedded_sdmmc::Mode::ReadWriteCreate) {
+        Ok(x) => x,
+        Err(_) => return Err(()),
+    };
+    info!("File created");
+    Ok(())
+}
 
 #[embassy_executor::task]
 pub async fn sdmmc_task(
@@ -41,38 +115,47 @@ pub async fn sdmmc_task(
         }
     }
 
-    let card = unwrap!(sdmmc.card());
+    let sdcard = MySdmmc::new(sdmmc);
+    let time_source = MyTs {};
+    let mut volume_mgr = embedded_sdmmc::VolumeManager::new(sdcard, time_source);
+    info!("Volume created");
+    match create_file(&mut volume_mgr) {
+        Ok(_) => info!("File success"),
+        Err(_) => info!("File fail"),
+    };
 
-    info!("Card: {:#?}", Debug2Format(card));
-    info!("Clock: {}", sdmmc.clock());
+    // let card = unwrap!(sdmmc.card());
 
-    // Arbitrary block index
-    let block_idx = 16;
+    // info!("Card: {:#?}", Debug2Format(card));
+    // info!("Clock: {}", sdmmc.clock());
 
-    // SDMMC uses `DataBlock` instead of `&[u8]` to ensure 4 byte alignment required by the hardware.
-    let mut block = DataBlock([0u8; 512]);
+    // // Arbitrary block index
+    // let block_idx = 16;
 
-    sdmmc.read_block(block_idx, &mut block).await.unwrap();
-    info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
+    // // SDMMC uses `DataBlock` instead of `&[u8]` to ensure 4 byte alignment required by the hardware.
+    // let mut block = DataBlock([0u8; 512]);
 
-    if !ALLOW_WRITES {
-        info!("Writing is disabled.");
-        loop {}
-    }
+    // sdmmc.read_block(block_idx, &mut block).await.unwrap();
+    // info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
 
-    info!("Filling block with 0x55");
-    block.fill(0x55);
-    sdmmc.write_block(block_idx, &block).await.unwrap();
-    info!("Write done");
+    // if !ALLOW_WRITES {
+    //     info!("Writing is disabled.");
+    //     loop {}
+    // }
 
-    sdmmc.read_block(block_idx, &mut block).await.unwrap();
-    info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
+    // info!("Filling block with 0x55");
+    // block.fill(0x55);
+    // sdmmc.write_block(block_idx, &block).await.unwrap();
+    // info!("Write done");
 
-    info!("Filling block with 0xAA");
-    block.fill(0xAA);
-    sdmmc.write_block(block_idx, &block).await.unwrap();
-    info!("Write done");
+    // sdmmc.read_block(block_idx, &mut block).await.unwrap();
+    // info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
 
-    sdmmc.read_block(block_idx, &mut block).await.unwrap();
-    info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
+    // info!("Filling block with 0xAA");
+    // block.fill(0xAA);
+    // sdmmc.write_block(block_idx, &block).await.unwrap();
+    // info!("Write done");
+
+    // sdmmc.read_block(block_idx, &mut block).await.unwrap();
+    // info!("Read: {=[u8]:X}...{=[u8]:X}", block[..8], block[512 - 8..]);
 }
